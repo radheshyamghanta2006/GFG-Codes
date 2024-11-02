da9ae914f1a37c8d149a9b660c1cbe13c58358ed
Radheshyam Ghanta <146259883+radheshyamghanta2006@users.noreply.github.com>
2024-11-03 01:08:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Kth distance/kth-distance.cpp
+++ Difficulty: Easy/Kth distance/kth-distance.cpp
@@ -11,13 +11,13 @@ class Solution {
 
     bool checkDuplicatesWithinK(vector<int>& arr, int k) {
         // your code
-        int n=arr.size();
-        unordered_map<int ,int>mp;
-        for(int i=0;i<n;i++){
-            if(mp[arr[i]]!=0) return true;
-            if(i-k>=0) mp[arr[i-k]]--;
-            mp[arr[i]]++;
-            
+        unordered_map<int,int>mp;
+        for(int i=0;i<arr.size();i++){
+            if(mp.find(arr[i])!=mp.end()){
+                if(i-mp[arr[i]]<=k) return true;
+                else mp[arr[i]]=i;
+            }
+            mp[arr[i]]=i;
         }
         return false;
     }
