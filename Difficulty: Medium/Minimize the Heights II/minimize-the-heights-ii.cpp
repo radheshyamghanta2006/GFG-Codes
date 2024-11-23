f9f89fe1a323fa7ce57dbdde8d4af4d62025e78d
Radheshyam Ghanta <146259883+radheshyamghanta2006@users.noreply.github.com>
2024-11-23 19:55:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimize the Heights II/minimize-the-heights-ii.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    int getMinDiff(vector<int> &arr, int k) {
+        // Code with Radheshyam (.^.)
+       int n=arr.size();
+        if(n==1) return 0;
+        sort(arr.begin(),arr.end());
+        int ans=arr[n-1]-arr[0];
+        for(int i=0;i<n-1;i++){
+            int mini=min(arr[0] + k, arr[i + 1] - k);
+            int maxi=max(arr[n-1]-k,arr[i]+k);
+            
+            if (mini < 0) continue;
+            ans = min(ans, maxi - mini);
+            
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        int n, k;
+        cin >> k;
+        cin.ignore();
+        vector<int> a, b, c, d;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num);
+
+        Solution ob;
+        auto ans = ob.getMinDiff(a, k);
+        cout << ans << "\n";
+        cout << '~' << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
