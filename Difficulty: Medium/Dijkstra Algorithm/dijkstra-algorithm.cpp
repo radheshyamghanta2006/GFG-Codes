372818c1735fbecade60550568a357baa01356f6
Radheshyam Ghanta <146259883+radheshyamghanta2006@users.noreply.github.com>
2025-04-11 23:06:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User Function Template
+class Solution {
+  public:
+    vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
+        // Code with Radheshyam (.^.)
+             vector<pair<int,int>>adj[V];
+        for(auto it:edges){
+            adj[it[0]].push_back({it[2], it[1]});
+            adj[it[1]].push_back({it[2], it[0]});
+        }
+        
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>>pq;
+        
+        vector<int>dist(V, 1e9);
+        dist[src]=0;
+        pq.push({0, src});
+        
+        while(!pq.empty()){
+            int wt=pq.top().first;
+            int node=pq.top().second;
+            pq.pop();
+            
+            for(auto it:adj[node]){
+                if(dist[it.second] > wt + it.first){
+                    dist[it.second] = wt+it.first;
+                    pq.push({dist[it.second], it.second});
+                }
+            }
+        }
+        for(int i=0;i<V;i++){
+            if(dist[i]==1e9) dist[i]=-1;
+        }
+        return dist;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int V, E;
+        cin >> V >> E;
+        vector<vector<int>> edges;
+        int i = 0;
+        while (i++ < E) {
+            int u, v, w;
+            cin >> u >> v >> w;
+            edges.push_back({u, v, w});
+            edges.push_back({v, u, w});
+        }
+        int src;
+        cin >> src;
+        cin.ignore();
+
+        Solution obj;
+        vector<int> res = obj.dijkstra(V, edges, src);
+
+        for (int i = 0; i < V; i++)
+            cout << res[i] << " ";
+        cout << endl;
+
+        cout << "~"
+             << "\n";
+    }
+
+    return 0;
+}
+// } Driver Code Ends
