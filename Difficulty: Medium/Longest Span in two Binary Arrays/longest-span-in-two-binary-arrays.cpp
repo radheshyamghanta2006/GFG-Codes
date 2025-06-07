76438f0ca4fccc4366b79dcfbd7b698a9fdd00ce
Radheshyam Ghanta <146259883+radheshyamghanta2006@users.noreply.github.com>
2025-06-07 23:35:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Span in two Binary Arrays/longest-span-in-two-binary-arrays.cpp
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+    int longestCommonSum(vector<int> &a1, vector<int> &a2) {
+        // Code with Radheshyam (.^.)
+        int n = a1.size();
+        unordered_map<int,int>mp;
+        
+        int sum1 = 0;
+        int sum2 = 0;
+        int result = 0;
+        
+        for(int i=0;i<n;i++){
+            sum1 += a1[i];
+            sum2 += a2[i];
+            
+            int diff = sum1 - sum2;
+            
+            if(diff == 0){
+                result = max(result, i+1);
+            } else if(mp.count(diff)){
+               result = max(result, i - mp[diff]);
+            } else {
+                mp[diff] = i;
+            }
+        }
+        return result;
+    }
+};
