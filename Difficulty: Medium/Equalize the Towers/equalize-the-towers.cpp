2d9340b4dfb138207a2654b4e7c2ab149e6725cd
Radheshyam Ghanta <146259883+radheshyamghanta2006@users.noreply.github.com>
2025-06-16 22:40:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Equalize the Towers/equalize-the-towers.cpp
@@ -0,0 +1,37 @@
+class Solution {
+  public:
+   
+   bool check(vector<int>& heights, vector<int>& cost, int mid){
+       long long costMid = 0;
+       long long costMidp1 = 0;
+        
+        for(int i =0;i < heights.size(); i++){
+            costMid += (long long) abs(heights[i] - mid) * cost[i];
+            costMidp1 += (long long) abs(heights[i] - (mid + 1)) * cost[i];
+        }
+        return costMid < costMidp1;
+   }
+  
+    int minCost(vector<int>& heights, vector<int>& cost) {
+        // Code with Radheshyam (.^.)
+        int low = *min_element(heights.begin(),heights.end());
+        int high = *max_element(heights.begin(),heights.end());
+        int targetH = 0;
+        
+        while(low <= high){
+            int mid = (low + high)/2;
+            if (check(heights, cost, mid)){
+                
+                targetH = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        long long result = 0;
+        for(int i = 0; i < heights.size(); i++){
+            result += (long long) abs(heights[i] - targetH) * cost[i];
+        }
+        return (int)result;
+    }
+};
