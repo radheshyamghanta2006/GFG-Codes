de5a11eea0a52bf14e268ac083c295404881bda3
Radheshyam Ghanta <146259883+radheshyamghanta2006@users.noreply.github.com>
2025-07-28 22:32:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Make Matrix Beautiful/make-matrix-beautiful.cpp
@@ -0,0 +1,30 @@
+class Solution {
+  public:
+    int balanceSums(vector<vector<int>>& mat) {
+        // Code with Radheshyam (.^.)
+        int n = mat.size(), maxsum=0,operations=0;
+        
+        for(int i=0;i<n;i++){
+            int sum = 0;
+            for(int j = 0;j < n;j++){
+                sum += mat[i][j];
+            }
+            maxsum = max(maxsum,sum);
+        }
+        for(int j=0;j<n;j++){
+            int sum = 0;
+            for(int i = 0;i < n;i++){
+                sum += mat[i][j];
+            }
+            maxsum = max(maxsum,sum);
+        }
+        for(int i=0;i<n;i++){
+            int rowsum = 0;
+            for(int j = 0;j < n;j++){
+                rowsum += mat[i][j];
+            }
+            operations += maxsum-rowsum;
+        }
+        return operations;
+    }
+};
