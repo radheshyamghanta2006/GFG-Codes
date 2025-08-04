3942073ce384398930b826f8673fdbe46f932656
Radheshyam Ghanta <146259883+radheshyamghanta2006@users.noreply.github.com>
2025-08-04 22:41:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Maximum sum Rectangle/maximum-sum-rectangle.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    int maxRectSum(vector<vector<int>> &mat) {
+        // Code with Radheshyam (.^.)
+        int n = mat.size(), m = mat[0].size(),finalmaxi = INT_MIN;
+        for(int start=0;start<m;start++){
+            vector<int>temp(n,0);
+            for(int end=start;end<m;end++){
+                for(int i=0;i<n;i++) temp[i]+=mat[i][end];
+                
+                int sum=0;int maxi = INT_MIN;
+                for(int i=0;i<n;i++){
+                    sum+=temp[i];
+                    maxi=max(maxi,sum);
+                    if(sum<0) sum=0;
+                }
+                finalmaxi=max(finalmaxi,maxi);
+            }
+        }
+        return finalmaxi;
+    }
+};
