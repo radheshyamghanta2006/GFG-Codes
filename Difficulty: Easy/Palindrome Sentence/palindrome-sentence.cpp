15cdcdf880aec93cea88749076d59e9e50914f80
Radheshyam Ghanta <146259883+radheshyamghanta2006@users.noreply.github.com>
2025-08-05 20:05:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Palindrome Sentence/palindrome-sentence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    bool isPalinSent(string &s) {
+        // Code with Radheshyam (.^.)
+        int start = 0;
+        int end = s.size()-1;
+        
+        while(start < end){
+            if(!isalnum(s[start])) start++;
+            else if(!isalnum(s[end])) end--;
+            
+            else if(tolower(s[start])==tolower(s[end])) start++,end--;
+            else return false;
+            
+        }
+        return true;
+    }
+};
