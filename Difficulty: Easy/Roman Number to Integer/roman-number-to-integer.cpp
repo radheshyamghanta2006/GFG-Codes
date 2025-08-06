85861218aa3d1832cb186529a6cefa49c29efd56
Radheshyam Ghanta <146259883+radheshyamghanta2006@users.noreply.github.com>
2025-08-06 20:47:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Roman Number to Integer/roman-number-to-integer.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    int romanToDecimal(string &s) {
+        // Code with Radheshyam (.^.)
+        int ans=0;
+        unordered_map<char,int>mp;
+        mp['I']=1;
+        mp['V']=5;
+        mp['X']=10;
+        mp['L']=50;
+        mp['C']=100;
+        mp['D']=500;
+        mp['M']=1000;
+        
+        for(int i = 0;i < s.size();i++){
+            if(s[i+1] && mp[s[i]] < mp[s[i+1]]){
+                ans-=mp[s[i]];
+            }
+            else ans+=mp[s[i]];
+        }
+        return ans;
+    }
+};
