13bc0d3464efcd47941109e39106cb6bb01014b2
Radheshyam Ghanta <146259883+radheshyamghanta2006@users.noreply.github.com>
2025-08-13 23:41:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Tywin's War Strategy/tywins-war-strategy.cpp
@@ -0,0 +1,30 @@
+class Solution {
+  public:
+    int minSoldiers(vector<int>& arr, int k) {
+        // Code with Radheshyam (.^.)
+        int n = arr.size();
+        int target = (n + 1) / 2; 
+        
+        int count = 0;
+        vector<int> req;
+        
+        for (int num : arr) {
+            if (num % k == 0) count++;
+            else req.push_back(k - (num % k));
+        }
+        
+        if (count >= target) return 0;
+        
+        sort(req.begin(), req.end());
+        
+        int res = 0;
+        int need = target - count;
+        
+        for (int i = 0; i < need; i++) {
+            res += req[i];
+        }
+        
+        return res;
+        
+    }
+};
