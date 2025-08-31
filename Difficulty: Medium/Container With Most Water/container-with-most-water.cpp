6cc2303ff6eae63dcc80f1a5e54fcdd360c0ddd8
Radheshyam Ghanta <146259883+radheshyamghanta2006@users.noreply.github.com>
2025-08-31 18:32:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Container With Most Water/container-with-most-water.cpp
+++ Difficulty: Medium/Container With Most Water/container-with-most-water.cpp
@@ -1,49 +1,22 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
-
   public:
     int maxWater(vector<int> &arr) {
-        // code with Radheshyam (.^.)
-        int s=0,e=arr.size()-1, maxarea=0, area=0;
-        while(s<e){
-            area=min(arr[s],arr[e])*(e-s);
-            maxarea=max(maxarea,area);
-            if(arr[s]<arr[e]) s++;
-            else e--;
+        // Code with Radheshyam (.^.)
+        int l = 0, r = arr.size() - 1;
+        int ans = 0;
+
+        while (l < r) {
+            int hgt = min(arr[l], arr[r]);
+            int wdt = r - l;
+            ans = max(ans, hgt * wdt);
+
+            if (arr[l] <= arr[r]) {
+                l++;
+            } else {
+                r--;
+            }
         }
-        return maxarea;
+        return ans;
+        
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-
-        // Read first array
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-
-        Solution ob;
-        int res = ob.maxWater(arr);
-
-        cout << res << endl << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
