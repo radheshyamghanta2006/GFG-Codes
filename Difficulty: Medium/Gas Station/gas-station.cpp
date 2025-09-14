b0a4f1387e35e14874b819ecbecb07534279155f
Radheshyam Ghanta <146259883+radheshyamghanta2006@users.noreply.github.com>
2025-09-14 12:59:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Gas Station/gas-station.cpp
+++ Difficulty: Medium/Gas Station/gas-station.cpp
@@ -1,72 +1,34 @@
-//{ Driver Code Starts
-// C++ program to find union of
-// two sorted arrays
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
-
-/*You are required to complete this method*/
 class Solution {
   public:
     int startStation(vector<int> &gas, vector<int> &cost) {
-        // Code with Radheshyam (.^.)
-        int total=0;
-        int req=0;
+        //  Code with Radheshyam (.^.)
+        
+        int ans = 0;
+        int currGas = 0;
+        int total = 0;
+        int req = 0;
+        
         for(int i=0;i<cost.size();i++){
-            req+=cost[i];
-            total+=gas[i];
+            // if the total gase required is greater than total gase have 
+            // then we should retrurn -1 because theare can't be any ans
+            req += cost[i];
+            total += gas[i];
         }
         if(total < req) return -1;
-            int ans=0;
-            int currgas=0;
-            for(int i=0;i<gas.size();i++){
-                currgas+=gas[i]-cost[i];
-                if(currgas<0){
-                    ans=i+1;
-                    currgas=0;
-                }
+        
+        
+        for(int i=0;i<gas.size();i++){
+            //Store the amount of gase for every gase station 
+            // after travel to the another gase station
+            currGas += gas[i] - cost[i];
+            
+            // If the currGas is less than 0 then its not 
+            //the ans then update the curr gase and ans
+            if(currGas < 0){
+                currGas = 0;
+                ans = i+1;
             }
+        }
         return ans;
     }
 };
-
-
-
-//{ Driver Code Starts.
-/* Driver program to test above function */
-int main() {
-    string ts;
-    getline(cin, ts);
-    int t = stoi(ts);
-
-    while (t--) {
-
-        vector<int> gas, cost;
-        string input1;
-        getline(cin, input1);
-        stringstream ss(input1);
-        int number1;
-        while (ss >> number1) {
-            gas.push_back(number1);
-        }
-        string input2;
-        getline(cin, input2);
-        stringstream sss(input2);
-        int number2;
-        while (sss >> number2) {
-            cost.push_back(number2);
-        }
-        Solution ob;
-        int ans = ob.startStation(gas, cost);
-
-        cout << ans << endl;
-        cout << "~\n";
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
