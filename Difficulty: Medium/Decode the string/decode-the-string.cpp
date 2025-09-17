fe7863cde80708b9b5c8762020ac6806ec96df22
Radheshyam Ghanta <146259883+radheshyamghanta2006@users.noreply.github.com>
2025-09-17 18:04:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Decode the string/decode-the-string.cpp
+++ Difficulty: Medium/Decode the string/decode-the-string.cpp
@@ -1,11 +1,3 @@
-//{ Driver Code Starts
-// Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 
 
 class Solution {
@@ -48,22 +40,3 @@ class Solution {
     }
 };
 
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        string s;
-        cin >> s;
-
-        Solution ob;
-        cout << ob.decodedString(s) << "\n";
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
