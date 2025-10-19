632172b9dc6ce2796bf140f01c54e5cbf73fa1f9
Radheshyam Ghanta <146259883+radheshyamghanta2006@users.noreply.github.com>
2025-10-19 19:32:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/K closest Values/k-closest-values.cpp
@@ -0,0 +1,63 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+*/
+
+class Solution {
+    vector<int>in;
+    private:
+    void inorder(Node* root) {
+        if (root == nullptr) return;
+        inorder(root->left);
+        in.push_back(root->data);
+        inorder(root->right);
+    }
+    
+    int getElementClosestToTarget(const vector<int>& arr, int target) {
+        int l = 0, h = (int)arr.size() - 1, idx = 0;
+        while (l <= h) {
+            int mid = l + (h - l) / 2;
+            if (arr[mid] <= target) {
+                idx = mid;
+                l = mid + 1;
+            } else {
+                h = mid - 1;
+            }
+        }
+        return idx;
+    }
+  public:
+    vector<int> getKClosest(Node* root, int target, int k) {
+        // Code with Radheshyam (.^.)
+        vector<int> res;
+        inorder(root);
+        int n = (int)in.size();
+        
+        int idx = getElementClosestToTarget(in, target);
+        
+        int l = idx, h = idx + 1;
+        while (k > 0) {
+            if (l >= 0 && (h == n || abs(in[l] - target) <= abs(in[h] - target))) {
+                res.push_back(in[l]);
+                l--;
+            } else {
+                res.push_back(in[h]);
+                h++;
+            }
+            k--;
+        }
+        
+        return res;
+        
+    }
+};
