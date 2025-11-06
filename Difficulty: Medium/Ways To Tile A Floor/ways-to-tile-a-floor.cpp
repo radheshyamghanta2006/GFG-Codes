5bc129c64d4d442a0ff07386434c1bcaf25de9c3
Radheshyam Ghanta <146259883+radheshyamghanta2006@users.noreply.github.com>
2025-11-06 22:02:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Ways To Tile A Floor/ways-to-tile-a-floor.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int numberOfWays(int n) {
+        // Code with Radheshyam (.^.)
+        int a = 0, b = 1, c = 0;
+        for(int i=2;i<=n+1;i++){
+            c = a+b;
+            a=b;
+            b=c;
+        }
+        return c;
+    }
+};
