7a467bd7665c201857c425272e27e62577115dab
Radheshyam Ghanta <146259883+radheshyamghanta2006@users.noreply.github.com>
2025-11-17 22:46:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Max Sum Increasing Subsequence/max-sum-increasing-subsequence.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    int maxSumIS(vector<int>& arr) {
+        // Code with Radheshyam (.^.)
+        int ans=0;
+        int n=arr.size();
+        vector<int>dp(n,0);
+        for(int i=0;i<n;i++)
+        {
+            dp[i]=max(dp[i],arr[i]);
+            for(int j=i-1;j>=0;j--)
+            {
+                if(arr[i]>arr[j])
+                { 
+                    dp[i]=max(dp[i],arr[i]+dp[j]);
+                    
+                }
+            }
+            ans=max(ans,dp[i]);
+        }
+        return ans;
+        
+    }
+};
