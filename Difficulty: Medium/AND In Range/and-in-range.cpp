af9a1ba739019cc30e3f53c1d54f0ffc5249c7a3
Radheshyam Ghanta <146259883+radheshyamghanta2006@users.noreply.github.com>
2025-11-26 23:03:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/AND In Range/and-in-range.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int andInRange(int l, int r) {
+        // Code with Radheshyam(.^.)
+        int count = 0;
+        while(l!=r){
+            l>>=1;
+            r>>=1;
+            count++;
+        }
+        return (l<<count);
+    }
+};
