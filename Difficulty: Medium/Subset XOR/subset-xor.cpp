136954f70992723424d03683d9b62c7916510019
Radheshyam Ghanta <146259883+radheshyamghanta2006@users.noreply.github.com>
2025-11-28 23:54:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subset XOR/subset-xor.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    vector<int> subsetXOR(int n) {
+        // Code with Radheshyam (.^.)
+        int xorr=0;
+        for(int i=1;i<=n;i++)
+        {
+            xorr^=i;
+        }
+        vector<int>ans;
+        int missing=-1;
+        if(xorr!=n)
+        {
+            missing=(n^xorr);
+        }
+        for(int i=1;i<=n;i++)
+        {
+            if(i!=missing)
+            {
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
