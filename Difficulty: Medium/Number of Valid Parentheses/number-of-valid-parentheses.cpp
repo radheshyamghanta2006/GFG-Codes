affaf11837250faf792579d0e53f8ab93cd032d0
Radheshyam Ghanta <146259883+radheshyamghanta2006@users.noreply.github.com>
2026-01-25 20:37:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Number of Valid Parentheses/number-of-valid-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    int findWays(int n) {
+        // Code with Radheshyam (.^.)
+        if(n%2!=0)
+        {
+            return 0;
+        }
+        n/=2;
+        vector<int>dp(n+1,0);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++)
+        {
+            int in=0;
+            int out=i-1;
+            while(in<i)
+            {
+                dp[i]+=(dp[in]*dp[out]);
+                in++;
+                out--;
+            }
+        }
+        return dp[n];
+    }
+};
