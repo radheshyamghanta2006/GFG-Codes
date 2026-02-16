2c386e5eaab3907e3884d18d7f47f37b46044452
Radheshyam Ghanta <146259883+radheshyamghanta2006@users.noreply.github.com>
2026-02-16 22:47:12 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Easy/Meeting Rooms/meeting-rooms.cpp
@@ -0,0 +1,12 @@
+// User function Template for C++
+class Solution {
+  public:
+    bool canAttend(vector<vector<int>> &arr) {
+        // Code with Radheshyam (.^.)
+         sort(arr.begin(),arr.end());
+        for(int i=0;i<arr.size()-1;i++){
+            if(arr[i][1]>arr[i+1][0]) return false;
+        }
+        return true;
+    }
+};
