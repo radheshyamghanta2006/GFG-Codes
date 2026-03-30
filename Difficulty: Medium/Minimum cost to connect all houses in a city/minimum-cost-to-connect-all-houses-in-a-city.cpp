b221ce7583d6ff119d12cb3eaae0bfa2454b72c3
Radheshyam Ghanta <146259883+radheshyamghanta2006@users.noreply.github.com>
2026-03-30 23:10:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum cost to connect all houses in a city/minimum-cost-to-connect-all-houses-in-a-city.cpp
+++ Difficulty: Medium/Minimum cost to connect all houses in a city/minimum-cost-to-connect-all-houses-in-a-city.cpp
@@ -1,11 +1,3 @@
-//{ Driver Code Starts
-// Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 
 class Solution {
   public:
@@ -55,32 +47,3 @@ class Solution {
     
 };
 
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        vector<vector<int>> edges;
-
-        for (int i = 0; i < n; ++i) {
-            vector<int> temp;
-            for (int j = 0; j < 2; ++j) {
-                int x;
-                cin >> x;
-                temp.push_back(x);
-            }
-            edges.push_back(temp);
-        }
-
-        Solution obj;
-
-        cout << obj.minCost(edges);
-        cout << "\n";
-        cout << "~" << endl;
-    }
-}
-
-// } Driver Code Ends
