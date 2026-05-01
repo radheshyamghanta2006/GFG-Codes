6e6e24ad472fbfde5c25ab8cedbdc21b52cf68bf
Radheshyam Ghanta <146259883+radheshyamghanta2006@users.noreply.github.com>
2026-05-01 22:43:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Kth Largest in a Stream/kth-largest-in-a-stream.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    vector<int> kthLargest(vector<int>& arr, int k) {
+        // Code with Radheshyam (.^.)
+        int n = arr.size();
+        vector<int> ans(n,-1);
+        priority_queue<int,vector<int>, greater<int>> pq;
+        for(int i = 0;i<n;i++)
+        {
+            pq.push(arr[i]);
+            if(pq.size()>k)
+            {
+                pq.pop();
+            }
+            if(pq.size() == k)
+            {
+                ans[i] = pq.top();
+            }
+        }
+        return ans;
+    }
+};
