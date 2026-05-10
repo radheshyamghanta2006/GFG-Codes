8ba3fd9cd23be9d0b95e9458ac236b57a82dcdfe
Radheshyam Ghanta <146259883+radheshyamghanta2006@users.noreply.github.com>
2026-05-10 22:42:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Max Profit from Two Machines/max-profit-from-two-machines.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+// Code with Radheshyam (.^.)
+    int maxProfit(int x, int y, vector<int> &a, vector<int> &b) {
+        int n = a.size();
+        long long totalProfit = 0;
+        vector<int> gains;
+
+        for (int i = 0; i < n; i++) {
+            totalProfit += b[i];
+            gains.push_back(a[i] - b[i]);
+        }
+
+        sort(gains.rbegin(), gains.rend());
+
+        int countA = 0;
+        int mandatoryA = max(0, n - y);
+        for (int i = 0; i < mandatoryA; i++) {
+            totalProfit += gains[i];
+            countA++;
+        }
+
+        for (int i = countA; i < x && i < n; i++) {
+            if (gains[i] > 0) {
+                totalProfit += gains[i];
+            } else {
+                break;
+            }
+        }
+
+        return (int)totalProfit;
+    }
+};
