6b3dacdd514121aed8d97648d864c1e0e54931c7
Radheshyam Ghanta <146259883+radheshyamghanta2006@users.noreply.github.com>
2026-06-02 22:32:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Pairs with certain difference/pairs-with-certain-difference.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    int sumDiffPairs(vector<int>& arr, int k) {
+        // Code with Radheshyam (.^.)
+        sort(arr.begin(), arr.end());
+
+        int ans = 0;
+        int i=arr.size()-1;
+
+        while(i>0)
+        {
+            if(arr[i]-arr[i-1] < k)
+            {
+                ans += (arr[i]+arr[i-1]);
+                i -= 2;
+            }
+            else 
+            {
+                i--;
+            }
+        }
+
+        return ans;
+    }
+};
