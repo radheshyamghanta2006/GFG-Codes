60bd45611ff36055e2305f735ac7182b86585e3c
Radheshyam Ghanta <146259883+radheshyamghanta2006@users.noreply.github.com>
2026-06-22 23:31:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Maximum Area Between Bars/maximum-area-between-bars.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    int maxArea(vector<int> &height) {
+        // Code with Radheshyam (.^.)
+        int i = 0 , j  = height.size()-1;
+        int ans=0;
+        while(i<j){
+            int length = j-i-1;
+            int h = min(height[i],height[j]);
+            
+            int area = length * h;
+            ans = max(ans,area);
+            if(height[i]<height[j]) i++;
+            else j--;
+            
+        }
+        return ans;
+        
+    }
+};
